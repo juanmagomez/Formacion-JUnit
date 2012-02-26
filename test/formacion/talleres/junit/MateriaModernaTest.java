a88841ad0425f00915c5c27d6f0631e467408755
Juanma Gomez <slither_new@hotmail.com>
2012-02-26 19:26:29 +0100

    Versión final tests MateriaModerna

--- test/formacion/talleres/junit/MateriaModernaTest.java
+++ test/formacion/talleres/junit/MateriaModernaTest.java
@@ -2,6 +2,7 @@ package formacion.talleres.junit;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,23 +21,53 @@ public class MateriaModernaTest {
 	}
 	
 	@Test
-	public void comprobarMateriaModernaTroncalNoEsObligatoria() {
+	public void materiaModernaTroncalNoEsObligatoria() {
 		assertFalse(materiaTroncal.isObligatoria());
 	}
 	
 	@Test
-	public void comprobarMateriaModernaTroncalEsObligatoria() {
+	public void materiaModernaTroncalEsObligatoria() {
 		assertFalse(materiaTroncal.isObligatoria());
 	}
 	
 	@Test
-	public void comprobarMateriaModernaObligatoriaEsObligatoria() {
+	public void materiaModernaObligatoriaEsObligatoria() {
 		assertTrue(materiaObligatoria.isObligatoria());
 	}
 	
 	@Test
-	public void comprobarMateriaModernaObligatoriaConLowerCaseEsObligatoria() {
+	public void materiaModernaObligatoriaConLowerCaseEsObligatoria() {
 		assertTrue(otraMateriaObligatoria.isObligatoria());
+	}
+	
+	@Test
+	public void materiaObligatoriaEstaMatriculada() {
+		materiaObligatoria.matricular();
+		assertTrue(materiaObligatoria.isMatriculada());
+	}
+	
+	@Test
+	public void materiaObligatoriaNoEstaPreMatriculada() {
+		materiaObligatoria.matricular();
+		assertFalse(materiaObligatoria.isPreMatriculada());
+	}
+	
+	@Test
+	public void materiaTroncalEstaPreMatriculada() {
+		materiaTroncal.matricular();
+		assertTrue(materiaTroncal.isPreMatriculada());
+	}
+	
+	@Test 
+	public void materiaTroncalNoEstaMatriculada() {
+		materiaTroncal.matricular();
+		assertFalse(materiaTroncal.isMatriculada());
+	}
+	
+	@Test
+	public void datosDeLaMateria() {
+		String programacion = "Programaci—n 6 T";
+		assertEquals(programacion, materiaTroncal.toString());
 	}
 	
 }
