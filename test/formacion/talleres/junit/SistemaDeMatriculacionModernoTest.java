69cd50fafc8bb267ca7c92c4479142578f871669
Juanma Gomez <slither_new@hotmail.com>
2012-03-07 21:37:46 +0100

    Some test refactor

--- test/formacion/talleres/junit/SistemaDeMatriculacionModernoTest.java
+++ test/formacion/talleres/junit/SistemaDeMatriculacionModernoTest.java
@@ -2,24 +2,33 @@ package formacion.talleres.junit;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SistemaDeMatriculacionModernoTest {
 
+	SistemaDeMatriculacionModerno sistemaDeMatriculacion;
+	AlumnoAventajado alumnoAventajado;
+	MateriaModerna materiaModerna;
+	List<MateriaModerna> materiasMatriculadas;
+	
+	@Before
+	public void setUp() {
+		sistemaDeMatriculacion = new SistemaDeMatriculacionModerno();
+		alumnoAventajado = mock(AlumnoAventajado.class);
+		materiaModerna = mock(MateriaModerna.class);
+		materiasMatriculadas = new ArrayList<MateriaModerna>();
+		materiasMatriculadas.add(materiaModerna);
+	}
+	
 	@Test
 	public void recuperamosMateriasMatriculadasDesAlumnoDentroDeGenerarResguardo() {
-		SistemaDeMatriculacionModerno sistemaDeMatriculacion = new SistemaDeMatriculacionModerno();
-		AlumnoAventajado alumnoAventajado = mock(AlumnoAventajado.class);
-		MateriaModerna materiaModerna = mock(MateriaModerna.class);
-		List<MateriaModerna> materiasMatriculadas = new ArrayList<MateriaModerna>();
-		materiasMatriculadas.add(materiaModerna);
-		
 		when(alumnoAventajado.obtenerNombreYApellidos()).thenReturn("");
 		when(materiaModerna.toString()).thenReturn("");
 		when(alumnoAventajado.getMateriasMatriculadas()).thenReturn(materiasMatriculadas);
@@ -31,16 +40,10 @@ public class SistemaDeMatriculacionModernoTest {
 	
 	@Test
 	public void generarResguardoTest() {
-		SistemaDeMatriculacionModerno sistemaDeMatriculacion = new SistemaDeMatriculacionModerno();
 		String SEPARADOR = sistemaDeMatriculacion.SALTO_DE_LINEA;
 		String NOMBRE_ALUMNO = "Perico el de los palotes";
 		String MATERIA_TO_STRING = "Materia 1";
 		String ALUMNO_Y_MATERIAS = NOMBRE_ALUMNO + SEPARADOR + MATERIA_TO_STRING + SEPARADOR;
-		
-		AlumnoAventajado alumnoAventajado = mock(AlumnoAventajado.class);
-		MateriaModerna materiaModerna = mock(MateriaModerna.class);
-		List<MateriaModerna> materiasMatriculadas = new ArrayList<MateriaModerna>();
-		materiasMatriculadas.add(materiaModerna);
 		
 		when(alumnoAventajado.obtenerNombreYApellidos()).thenReturn(NOMBRE_ALUMNO);
 		when(materiaModerna.toString()).thenReturn(MATERIA_TO_STRING);
